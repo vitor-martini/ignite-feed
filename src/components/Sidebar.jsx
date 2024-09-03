449733d6505f1574b91a35e3d7c6240aa8b047fa
Vitor Martini <vitor-martini@outlook.com>
2024-09-02 21:36:06 -0300

    feat: sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import styles from './Sidebar.module.css'
+import { PencilLine } from "phosphor-react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        src='https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
+        className={styles.cover}  
+      />
+      <div className={styles.profile}>
+        <img  
+          src='https://github.com/vitor-martini.png'
+          className={styles.avatar}  
+        />
+        <strong>Vitor Martini</strong>
+        <span>FullStack Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
