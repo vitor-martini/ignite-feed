9fcacef6a1f20aa8f473e148d49a21eb20acb858
Vitor Martini <vitor-martini@outlook.com>
2024-09-03 21:04:54 -0300

    feat: post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -5,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img 
-            src='https://github.com/vitor-martini.png'
-            className={styles.avatar}
-          />
+          <Avatar src="https://github.com/vitor-martini.png"/>
           <div className={styles.authorInfo}>
             <strong>Vitor Martini</strong>
             <span>FullStack Developer</span>
@@ -32,6 +31,22 @@ export function Post() {
           <a href="#">#nlw</a> {' '}
           <a href="#">#rocketseat</a> {' '}
         </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea 
+          placeholder='Deixe um comentário'
+        />
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment/>
+        <Comment/>
+        <Comment/>
       </div>
     </article>
   )
